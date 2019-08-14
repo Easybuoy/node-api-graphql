a9894534d57575e3d11d3c7f1fa651d04610c6fd
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-08-14 11:36:04 +0100

    add migrations

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    url: process.env.DB_URL,
+    dialect: "postgres"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  }
+};
---
+++ models/student.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Student = sequelize.define('Student', {
+    firstName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {});
+  Student.associate = function(models) {
+    // associations can be defined here
+    Student.hasMany(models.Hobbies)
+  };
+  return Student;
+};
